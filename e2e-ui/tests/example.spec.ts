d6877b1f05215ca6c8c39e2a2662d1e53dfd9c68
Arisa <arisa@welovebug.biz>
2026-02-23 11:33:15 +0700

    กรอกUsername และ password

--- e2e-ui/tests/example.spec.ts
+++ e2e-ui/tests/example.spec.ts
@@ -1,8 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('Login สำเร็จด้วย username เท่ากับ user_70 password เท่ากับ P@ssw0rd ', async ({ page }) => {
+test('Login สำเร็จด้วย username เท่ากับ user_70 password เท่ากับ P@ssw0rd', async ({ page }) => {
   //เปิดหน้า Website
   await test.step("เปิด Browser แล้วไปยังหน้าที่จะทดสอบ", async() => {
     await page.goto("http://139.59.225.96/auth/login");
     });
+    //กรอก User Name
+  await test.step("กรอก User Name", async() => {
+    await page.locator("#login-username-input").fill("user-70");
+  });
+  //กรอก Password
+    await test.step("กรอก Password", async() => {
+    await page.locator("#login-password-input").fill("P@ssw0rd");
+  });
+  //กดปุ่ม Login
+  await test.step("กดปุ่ม Login", async () => {
+    await page.locator("#login-btn").click();
+  });
+  //ตรวจสอบว่า Login สำเร็จและไปหน้าถัดไป
+
 });
